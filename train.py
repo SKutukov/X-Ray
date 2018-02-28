ef5a52c982d5334cdf10f8d33b42447ede9561a0
Sergey Kutukov <skutukov96@gmail.com>
2018-02-28 21:03:18 +0300

    smal cosmetical changes

--- train.py
+++ train.py
@@ -13,56 +13,55 @@ import numpy as np
 from autoencoder import autoencoder
 from epoch_callback import on_epoch_end
 from save_model import save_model
+import json
 
 if not os.path.exists('./dc_img'):
     os.mkdir('./dc_img')
 
-num_epochs = 15
-batch_size = 3
 learning_rate = 1e-3
-size = 1024
-
-
+size = 512
+    
 def cv2_loader(filename):
     im = cv2.imread(filename, 0)
     im = cv2.resize(im,(size,size),interpolation = cv2.INTER_AREA)
     im = im.reshape(size, size, 1)
     return im
 
-img_transform = transforms.Compose([
+
+if __name__ == '__main__':
+        #geting params
+    config_name = './config.json'
+    with  open(config_name):
+        data = json.load(open(config_name))
+        train_directory = data["train_directory"]
+        batch_size = data["batch_size"] 
+        num_epochs = data["num_epochs"]
+
+    img_transform = transforms.Compose([
     transforms.ToPILImage(),
     transforms.ToTensor(),
     # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
-dir = "/home/skutukov96/cloud/sample_images/test"
-dataset = ImageFolder(dir, loader=cv2_loader, transform=img_transform)
-# print(dataset.classes)
-# print(dataset.imgs)
-# dataset = MNIST('./data', transform=img_transform)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    ])
+    dataset = ImageFolder(train_directory, loader=cv2_loader, transform=img_transform)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 
-model = autoencoder()#.cuda()
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
+    model = autoencoder()#.cuda()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
                              weight_decay=1e-5)
-# for epoch in range(num_epochs):
-#     for i, (input, target) in enumerate(dataloader):
-#         img = Variable(input)
-#         print(img)
-#         exec(0)
-for epoch in range(num_epochs):
-    for data in dataloader:
-        img, _ = data
-        # print(img.shape)
-        img = Variable(img)#.cuda()
-        # ===================forward=====================
-        output = model(img)
-        loss = criterion(output, img)
-        # ===================backward====================
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    # ===================log========================
-    on_epoch_end(epoch, num_epochs, loss, output, size)        
-    save_model(epoch, model)    
+
+    for epoch in range(num_epochs):
+        for data in dataloader:
+            img, _ = data
+            img = Variable(img)#.cuda()
+            # ===================forward=====================
+            output = model(img)
+            loss = criterion(output, img)
+            # ===================backward====================
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        # ===================log========================
+        on_epoch_end(epoch, num_epochs, loss, output, size)        
+        save_model(epoch, model)    
